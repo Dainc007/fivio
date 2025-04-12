379332c5ec0f4a367b8038f1a150fc946bc0ac26
Daniel Heinze <danielheinze96@gmail.com>
2025-04-12 10:35:13 +0200

    pulse

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,7 +42,7 @@ final class User extends Authenticatable implements FilamentUser
 
     public function isAdmin(): bool
     {
-        return auth()->user()->is_admin;
+        return auth()->user()->is_admin === 1;
     }
 
     public function canAccessPanel(Panel $panel): bool
